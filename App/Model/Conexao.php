ee0b5326f605536038bd57c2cc0997b358d39c37
trabalho-final-faculdade <karininha.dias.18@hotmail.com>
2020-04-20 18:25:46 -0300

    alterado dados de conecao banco

--- App/Model/Conexao.php
+++ App/Model/Conexao.php
@@ -1,14 +1,13 @@
 <?php
 
 
-  $localhost = "localhost";
-  $user = "root";
-  $passw = "";
-  $banco = "sistema_pesquisa";
+  $localhost = "us-cdbr-iron-east-01.cleardb.net";
+  $user = "ba1afdaa9340aa";
+  $passw = "10ebf093";
+  $banco = "heroku_bdc5eb13438a9c1";
 
   global $pdo;
   try{
-
     $pdo = new PDO("mysql:dbname=".$banco."; host=".$localhost, $user, $passw);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }catch(PDOException $e){
