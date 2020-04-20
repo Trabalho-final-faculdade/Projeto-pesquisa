5592126737b2f9b04812e6ac71753f69509521fd
trabalho-final-faculdade <karininha.dias.18@hotmail.com>
2020-04-20 17:39:31 -0300

    alterado tela index

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 			<nav id="menu">
 				<ul class="links">
 					<li><a href="index.php">Página inicial</a></li>
-					<li><a href="sistema/tela-login.php">Logar</a></li>
+					<li><a href="App/View/sistema/tela-login.php">Logar</a></li>
 					<li><a href="elements.html">Pesquisas abertas</a></li>
 					<li><a href="elements.html">Quem somos</a></li>
 					<li><a href="elements.html">Dúvidas frequentes</a></li>
